9d4f5e02f22959d3e23e8aea5a6e6057e09d1127
Jonathan Lin <69509330+Jonathan-Lin-android@users.noreply.github.com>
2020-08-14 01:39:11 -0700

    opening a web page with implicit intent.

--- app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -17,6 +17,7 @@ package com.example.android.explicitintent;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -68,4 +69,66 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    /**
+     * This method is called when the Open Website button is clicked. It will open the website
+     * specified by the URL represented by the variable urlAsString using implicit Intents.
+     *
+     * @param v Button that was clicked.
+     */
+    public void onClickOpenWebpageButton(View v) {
+        String urlAsString = "http://www.udacity.com";
+        openWebPage(urlAsString);
+    }
+
+    /**
+     * This method is called when the Open Location in Map button is clicked. It will open the
+     * a map to the location represented by the variable addressString using implicit Intents.
+     *
+     * @param v Button that was clicked.
+     */
+    public void onClickOpenAddressButton(View v) {
+        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * This method is called when the Share Text Content button is clicked. It will simply share
+     * the text contained within the String textThatYouWantToShare.
+     *
+     * @param v Button that was clicked.
+     */
+    public void onClickShareTextButton(View v) {
+        Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * This is where you will create and fire off your own implicit Intent. Yours will be very
+     * similar to what I've done above. You can view a list of implicit Intents on the Common
+     * Intents page from the developer documentation.
+     *
+     * @see <http://developer.android.com/guide/components/intents-common.html/>
+     *
+     * @param v Button that was clicked.
+     */
+    public void createYourOwn(View v) {
+        Toast.makeText(this,
+                "TODO: Create Your Own Implicit Intent",
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    //implicit intent
+    private void openWebPage(String url)
+    {
+        Uri webpage = Uri.parse(url);
+        // action_view tells the android system that we want to view content
+        // content is a web page
+        Intent intent = new Intent (Intent.ACTION_VIEW, webpage);
+
+        // is there an app that can handle the request
+        if(intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
+
 }
